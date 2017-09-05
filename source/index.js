46325c6caccc9441880ad25fbc083506436aa62e
nick <nick@Mac-mini-nick.local>
2017-09-05 14:48:57 +0300

    add popup engine

--- source/index.js
+++ source/index.js
@@ -1,11 +1,18 @@
 (function(app){
 	'use strict';
 
-	app.controller('city-search', class{
-		constructor($scope){
+	app.controller('city-search', ['$scope', 'popup', class{
+		constructor($scope, $popup){
 			$scope.selectedcity = '';
+
+			$scope.onClick = function(){
+				console.log('Click2');
+				$popup.add({
+					title: 'Popup ' + Math.random()
+				});
+			}
 		}
-	});
+	}]);
 
 	function debounce(_cb, _msec){
 		var _pending;
@@ -73,5 +80,70 @@
 		},
 		template: '<div class="search-select_variant-inner" ng-click="$ctrl.onSelect($ctrl.data)">{{$ctrl.data.name}}</div>',
 	});
+
+//--------------------------------------------------------------------------------
+
+	// Implements popup engine
+
+	app.factory('popup', class{
+		constructor(){
+
+		}
+		add(conf){
+			if(this.root){
+				this.root.stack.push(conf);
+			}else{
+				console.warn('Popup factory haven\'t binded scope');
+			}
+		}
+		bindRoot(scope){
+			this.root = scope;
+		}
+	})
+	app.component('popupWrap', {
+		bindings:{
+			settings: '<',
+			onClose: '&',
+		},
+		controller: class{
+			constructor($scope, $element, $attrs){
+				console.log('INIT popup-wrap');
+				console.dir(arguments);
+				this.scope = $scope;
+			}
+			close(){
+				this.onClose();
+			}
+		},
+		template: `
+			<div class="ui-ppp_content">
+				<div style="border: 1px solid red;">{{$ctrl.settings.title}}</div>
+				<div ng-click="$ctrl.close()">[x]</div>
+			</div>
+		`,
+	});
+	// Host conatiner for popups
+	app.component('popupRoot', {
+		controller: ['$scope', '$element', '$attrs', 'popup', class{
+			constructor($scope, $element, $attrs, $popup){
+				this.stack = [];
+				this.scope = $scope;
+				$popup.bindRoot(this);
+			}
+			// Remove child popup
+			remove(data){
+				var pos = this.stack.findIndex(item => {
+					return item.$$hashKey == data.$$hashKey;
+				});
+
+				if(pos != -1){
+					this.stack.splice(pos, 1);
+				}
+			}
+		}],
+		template: '<popup-wrap class="ui-ppp" ng-repeat="data in $ctrl.stack" settings="data" on-close="$ctrl.remove(data)"></popup-wrap>',
+	});
+
+//--------------------------------------------------------------------------------
 
 }(angular.module('app', [])));
